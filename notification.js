12b3a6514682b1c767b8e30128052d3f902ab11c
beganscandal <152467917+beganscandal@users.noreply.github.com>
2026-05-22 02:22:14 +0700

    Update notification.js

--- notification.js
+++ notification.js
@@ -6,7 +6,55 @@ console.log(
 // CONFIG
 // =========================
 
-const NOTIFICATION_API = "https://script.google.com/macros/s/AKfycbza5dLHKWO8iizfjp_8CaT_QsNQ50e4zxf40mt_mfinlvhnMqCwpLxdrrBh7fEc79Fs/exec?action=notificationStatus";
+const NOTIFICATION_API =
+"https://script.google.com/macros/s/AKfycbza5dLHKWO8iizfjp_8CaT_QsNQ50e4zxf40mt_mfinlvhnMqCwpLxdrrBh7fEc79Fs/exec?action=notificationStatus";
+
+// =========================
+// SOUND
+// =========================
+
+const newDropSound =
+new Audio(
+  "https://cdn.prod.website-files.com/69c14cdea8e1d469f0564d69/6a07b03b3076e93739d1d7bb_new%20article%20drop.mp3"
+);
+
+newDropSound.volume = 0.8;
+
+// =========================
+// AUDIO UNLOCK
+// =========================
+
+function unlockNotifAudio(){
+
+  newDropSound.muted = true;
+
+  newDropSound.play()
+
+  .then(()=>{
+
+    newDropSound.pause();
+
+    newDropSound.currentTime = 0;
+
+    newDropSound.muted = false;
+
+  })
+
+  .catch(()=>{});
+}
+
+document.addEventListener(
+  "click",
+  unlockNotifAudio,
+  { once:true }
+);
+
+document.addEventListener(
+  "touchstart",
+  unlockNotifAudio,
+  { once:true }
+);
+
 // =========================
 // LAST VERSION
 // =========================
@@ -29,7 +77,10 @@ checkArticleUpdate(){
 
     const res =
       await fetch(
-        NOTIFICATION_API
+
+        NOTIFICATION_API +
+        "&t=" + Date.now()
+
       );
 
     const data =
@@ -39,6 +90,9 @@ checkArticleUpdate(){
       Number(
         data.article || 0
       );
+    
+    window.CURRENT_ARTICLE_VERSION =
+currentVersion;
 
     console.log({
 
@@ -47,7 +101,10 @@ checkArticleUpdate(){
 
     });
 
+    // =========================
     // ADA ARTIKEL BARU
+    // =========================
+
     if(
       currentVersion >
       LAST_ARTICLE_VERSION
@@ -56,6 +113,14 @@ checkArticleUpdate(){
       console.log(
         "ARTIKEL BARU TERDETEKSI"
       );
+
+      localStorage.setItem(
+        "articleUnread",
+        "true"
+      );
+
+      newDropSound.play()
+      .catch(()=>{});
 
     }
 
