706e6ae131c70f1861992011079078513a809021
beganscandal <152467917+beganscandal@users.noreply.github.com>
2026-06-08 01:42:14 +0700

    Update forum-render.js

--- forum/forum-render.js
+++ forum/forum-render.js
@@ -1,5 +1,25 @@
 function renderPosts(posts) {
 
-  console.log('POSTS:', posts);
+  const feed =
+    document.getElementById('forum-feed');
 
-} 
+  if (!feed) {
+    console.error('forum-feed not found');
+    return;
+  }
+
+  feed.innerHTML = posts.map(post => `
+    <div class="forum-post">
+      <div class="forum-post-header">
+        <strong>${post.partnerName}</strong>
+      </div>
+
+      <h3>${post.title}</h3>
+
+      <p>${post.content}</p>
+
+      <small>${post.category}</small>
+    </div>
+  `).join('');
+
+}
