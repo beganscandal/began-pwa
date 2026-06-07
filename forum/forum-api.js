8815ee821d1b5e5a4c27c5c19513e9b660ad3a4d
beganscandal <152467917+beganscandal@users.noreply.github.com>
2026-06-08 01:23:19 +0700

    Update forum-api.js

--- forum/forum-api.js
+++ forum/forum-api.js
@@ -1,1 +1,12 @@
+const API_URL =
+  'https://script.google.com/macros/s/AKfycby-6JRp4k_wDbOplmY6Rn8wH0q6LEdlpw48I04KiHjovKI2mi2qI3fw3a-du1lVtE7j8w/exec';
 
+async function getBoard() {
+
+  const response = await fetch(
+    API_URL + '?action=getBoard'
+  );
+
+  return response.json();
+
+}
