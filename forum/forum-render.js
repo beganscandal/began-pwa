2725bfec66e35ae384e63a30beec8de5b986eb0b
beganscandal <152467917+beganscandal@users.noreply.github.com>
2026-06-08 02:07:17 +0700

    Update forum-render.js

--- forum/forum-render.js
+++ forum/forum-render.js
@@ -23,3 +23,37 @@ function renderPosts(posts) {
   `).join('');
 
 }
+
+function renderBoardPosts(posts) {
+
+  const feed =
+    document.getElementById('dynamic-feed');
+
+  if (!feed) {
+    console.error('dynamic-feed not found');
+    return;
+  }
+
+  feed.innerHTML = posts.map(post => `
+    <div class="forum-card">
+
+      <div class="forum-author">
+        ${post.partnerName}
+      </div>
+
+      <div class="forum-category">
+        ${post.category}
+      </div>
+
+      <h3>
+        ${post.title}
+      </h3>
+
+      <p>
+        ${post.content}
+      </p>
+
+    </div>
+  `).join('');
+
+}
