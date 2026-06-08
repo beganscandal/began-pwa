6226edb4d4b220421678399db5db2f1f22f17eec
beganscandal <152467917+beganscandal@users.noreply.github.com>
2026-06-08 22:33:51 +0700

    Update post-media.js

--- forum/post/post-media.js
+++ forum/post/post-media.js
@@ -1,1 +1,22 @@
+(function(window){
 
+'use strict';
+
+function renderPostMedia(post){
+
+    if(!post.mediaUrl){
+        return '';
+    }
+
+    return `
+        <img
+            class="post-image"
+            src="${post.mediaUrl}"
+            alt=""
+        >
+    `;
+}
+
+window.renderPostMedia = renderPostMedia;
+
+})(window);
