47b16a207ef28f795603d75f27ae3f2dd1b07210
beganscandal <152467917+beganscandal@users.noreply.github.com>
2026-06-08 23:51:46 +0700

    Update forum-api.js

--- forum/forum-api.js
+++ forum/forum-api.js
@@ -1,25 +1,32 @@
 const API_URL =
   'https://script.google.com/macros/s/AKfycbxgrLYCtxbHDwRfozT4xBDVBq7JS7c847AkA_pYwNdOoxCNH-9_vnhJgcfLrE1DDAO0rQ/exec';
 
-async function getBoard() {
+window.ForumAPI = {
 
-  const response = await fetch(
-    API_URL + '?action=getBoard'
-  );
+  async getBoard() {
 
-  return response.json();
+    const response =
+      await fetch(
+        API_URL +
+        '?action=getBoard'
+      );
 
-}
+    return response.json();
 
-async function getPost(postId) {
+  },
 
-  const response = await fetch(
-    API_URL +
-    '?action=getPost' +
-    '&postId=' +
-    encodeURIComponent(postId)
-  );
+  async getPost(postId) {
 
-  return response.json();
+    const response =
+      await fetch(
+        API_URL +
+        '?action=getPost' +
+        '&postId=' +
+        encodeURIComponent(postId)
+      );
 
-}
+    return response.json();
+
+  }
+
+};
