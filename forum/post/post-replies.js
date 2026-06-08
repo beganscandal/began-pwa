8cbc1ccebc98a5d8fbe8c0558584bc8464d1bb39
beganscandal <152467917+beganscandal@users.noreply.github.com>
2026-06-08 22:34:12 +0700

    Update post-replies.js

--- forum/post/post-replies.js
+++ forum/post/post-replies.js
@@ -1,1 +1,29 @@
+(function(window){
 
+'use strict';
+
+function renderReplies(replies){
+
+    if(!replies.length){
+
+        return `
+            <div class="empty-state">
+                No replies yet
+            </div>
+        `;
+    }
+
+    return replies.map(function(reply){
+
+        return `
+            <div class="reply-card">
+                ${reply.content}
+            </div>
+        `;
+
+    }).join('');
+}
+
+window.renderReplies = renderReplies;
+
+})(window);
