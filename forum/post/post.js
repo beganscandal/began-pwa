64d04787608fe81036aa874b724e38f0fa920af8
beganscandal <152467917+beganscandal@users.noreply.github.com>
2026-06-08 19:16:44 +0700

    Update post.js

--- forum/post/post.js
+++ forum/post/post.js
@@ -1,1 +1,16 @@
-test
+(async function(){
+
+  const params =
+    new URLSearchParams(
+      location.search
+    );
+
+  const postId =
+    params.get('id');
+
+  console.log(
+    'POST ID:',
+    postId
+  );
+
+})();
