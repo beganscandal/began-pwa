f26e658c035535848aeb60596fb1787ab0508856
beganscandal <152467917+beganscandal@users.noreply.github.com>
2026-06-17 15:40:31 +0700

    Update notification.js

--- notifications/notification.js
+++ notifications/notification.js
@@ -1,2 +1,27 @@
+const filterContainer =
+document.getElementById(
+  "filter-container"
+);
 
+if(filterContainer){
+
+  setTimeout(()=>{
+
+    filterContainer.scrollTo({
+      left:80,
+      behavior:"smooth"
+    });
+
+    setTimeout(()=>{
+
+      filterContainer.scrollTo({
+        left:0,
+        behavior:"smooth"
+      });
+
+    },500);
+
+  },800);
+
+}
  
