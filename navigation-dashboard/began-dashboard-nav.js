f3ff7daf2b58bcb038c83d531dfe54ab21745937
beganscandal <152467917+beganscandal@users.noreply.github.com>
2026-06-29 06:22:20 +0700

    Update began-dashboard-nav.js

--- navigation-dashboard/began-dashboard-nav.js
+++ navigation-dashboard/began-dashboard-nav.js
@@ -80,26 +80,6 @@ Dashboard
 
 </button>
 
-<div class="
-filter-dropdown
-began-nav-kategori">
-
-<button
-class="began-nav__item began-nav__kategori-btn"
-data-nav="kategori">
-
-<span class="began-nav__label">
-KATEGORI PRODUCT
-</span>
-
-<img
-class="began-nav__icon"
-src="https://pwa.barkahgarment.com/assets/toggleDown.jpg">
-
-</button>
-
-</div>
-
 <button
 class="began-nav__item"
 data-nav="reserve">
