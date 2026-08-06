812ad3e55942a869c95eb669b6a5a7153756472d
beganscandal <152467917+beganscandal@users.noreply.github.com>
2026-08-06 17:24:38 +0700

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './forum/**/*.html',
+    './forum/**/*.js'
+  ],
+
+  theme: {
+    extend: {}
+  },
+
+  plugins: []
+};
